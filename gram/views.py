9520a359411f9f0b2430aad26754763a0dff09be
[muneneee] <[lukekevin39@gmail.com]>
2020-05-30 16:06:44 +0300

    Feature - Registartion 	-create registration form 	-create registration view 	-define register url

--- gram/views.py
+++ gram/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from .models import Image
 
 
@@ -9,3 +10,15 @@ def index(request):
     photos = Image.objects.all()
 
     return render(request, 'insta/index.html', {'photos':photos})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html',{'form':form})
