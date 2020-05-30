a70ae8a6b93d1544eb2e4d12f39c5e2a004fdb81
[muneneee] <[lukekevin39@gmail.com]>
2020-05-31 00:18:39 +0300

    Feature - Profile 	-create a profile form 	-create one to one field with user and profile

--- gram/models.py
+++ gram/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from pyuploadcare.dj.models import ImageField
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 
 
-class Profile:
+class Profile(models.Model):
     profile_photo = ImageField(blank = True, manual_crop="")
     bio = models.TextField()
+    account = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 
 
@@ -24,8 +27,9 @@ class Image(models.Model):
     image = ImageField(blank = False, manual_crop="")
     name = models.CharField(max_length = 20)
     caption= models.TextField()
-    #profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     posted = timezone.now()
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 
