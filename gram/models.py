2f563d4a18513a9762ed08ea9086fd3c9d994bcc
[muneneee] <[lukekevin39@gmail.com]>
2020-05-30 13:35:23 +0300

    Feature - Profile 	-add a new model, profile 	-create a relationnship between the image and the profile

--- gram/models.py
+++ gram/models.py
@@ -4,8 +4,21 @@ from pyuploadcare.dj.models import ImageField
 
 
 class Image(models.Model):
-    Image = ImageField(blank = True, manual_crop="")
+    Image = ImageField(blank = False, manual_crop="")
     name = models.CharField(max_length = 20)
     caption= models.TextField()
-    
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile:
+    profile_photo = ImageField(blank = True, manual_crop="")
+    bio = models.TextField()
+
+
+    def __str__(self):
+        return self.bio
     
